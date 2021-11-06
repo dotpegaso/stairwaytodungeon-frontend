92b27a4af241bb3bfb28328bbe59487d727d0cab
dotpegaso <dotpegaso@gmail.com>
2021-11-06 02:32:41 -0300

    hue

--- src/components/CharacterPool/index.js
+++ src/components/CharacterPool/index.js
@@ -1,18 +1,25 @@
 import * as S from './styles'
+import _ from 'lodash'
 import Link from 'next/link'
 
 import CharacterName from '../CharacterName'
 
-const CharacterPool = ({ characters }) => (
-  <S.Container>
-    {characters.map((character, index) => (
-      <Link key={index} href={`/characters/${character.id}?option=character`}>
-        <S.CharacterCard>
-          <CharacterName>{character.name}</CharacterName>
-        </S.CharacterCard>
-      </Link>
-    ))}
-  </S.Container>
-)
+const CharacterPool = ({ characters }) => {
+  const aliveCharacters = _.defaultTo(characters, []).filter((character) =>
+    _.get(character, 'isAlive')
+  )
+
+  return (
+    <S.Container>
+      {aliveCharacters.map((character, index) => (
+        <Link key={index} href={`/characters/${character.id}?option=character`}>
+          <S.CharacterCard>
+            <CharacterName>{character.name}</CharacterName>
+          </S.CharacterCard>
+        </Link>
+      ))}
+    </S.Container>
+  )
+}
 
 export default CharacterPool
